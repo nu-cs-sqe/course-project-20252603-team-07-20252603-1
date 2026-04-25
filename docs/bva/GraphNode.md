3a892eedf06d0f8fd87d976a1b1ee018bd217fb9
SpencerD1123 <155921740+SpencerD1123@users.noreply.github.com>
2026-04-25 18:17:34 -0500

    claimGraphNodeOccupied_ExpectError

--- docs/bva/GraphNode.md
+++ docs/bva/GraphNode.md
@@ -4,7 +4,7 @@
   - **State of the system**: Player attempts to claim an unoccupied Node
   - **Expected output**: True, node now has input color
 
-- **TC2: claimGraphNodeOccupied_ExpectError** (NOT_IMPLEMENTED)
+- **TC2: claimGraphNodeOccupied_ExpectError** (IMPLEMENTED)
   - **State of the system**: Player attempts to claim an occupied node
-  - **Expected output**: Error, node still has same state as before
+  - **Expected output**: Error "Node Already Claimed", node still has same state as before
 
--- src/test/java/domain/GraphNodeTests.java
+++ src/test/java/domain/GraphNodeTests.java
@@ -1,13 +1,14 @@
 package domain;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GraphNodeTests {
+
     @ParameterizedTest
     @EnumSource(value = PlayerColor.class, names = {"RED", "WHITE", "ORANGE", "BLUE"})
     void claimGraphNode_NodeUnoccupied_ExpectTrue(PlayerColor color) {
@@ -15,5 +16,17 @@ public class GraphNodeTests {
         assertTrue(g1.playerClaimNode(color));
         assertTrue(g1.checkOccupied());
         assertEquals(color, g1.checkColor());
+    }
+
+    @Test
+    void claimGraphNodeOccupied_ExpectError() {
+        GraphNode g1 = new GraphNode(0);
+        g1.playerClaimNode(PlayerColor.BLUE);
+        assertTrue(g1.checkOccupied());
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> g1.playerClaimNode(PlayerColor.ORANGE));
+        assertEquals("Node Already Claimed", exception.getMessage());
+        assertTrue(g1.checkOccupied());
+        assertEquals(PlayerColor.BLUE, g1.checkColor());
     }
 }
