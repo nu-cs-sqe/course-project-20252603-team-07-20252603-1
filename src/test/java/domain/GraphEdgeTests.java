c49c8372245794b7ae297220a0ad579090cabe42
SpencerD1123 <155921740+SpencerD1123@users.noreply.github.com>
2026-04-26 23:14:39 -0500

    Updated tests to fit with refactored GraphEdge class

--- src/test/java/domain/GraphEdgeTests.java
+++ src/test/java/domain/GraphEdgeTests.java
@@ -10,7 +10,7 @@ public class GraphEdgeTests {
     @ParameterizedTest
     @EnumSource(value = PlayerColor.class, names = {"RED", "WHITE", "ORANGE", "BLUE"})
     void claimGraphEdge_NodeUnoccupied_ExpectTrue(PlayerColor color) {
-        GraphEdge e1 = new GraphEdge(0);
+        GraphEdge e1 = new GraphEdge(0, 1);
         assertTrue(e1.claimGraphEdge(color));
         assertTrue(e1.checkRoadExists());
         assertEquals(color, e1.checkOwningColor());
@@ -19,7 +19,7 @@ public class GraphEdgeTests {
 
     @Test
     void claimGraphEdge_EdgeUnoccupied_ExpectError() {
-        GraphEdge e1 = new GraphEdge(0);
+        GraphEdge e1 = new GraphEdge(52, 53);
         e1.claimGraphEdge((PlayerColor.BLUE));
 
         Exception exception = assertThrows(IllegalArgumentException.class,
