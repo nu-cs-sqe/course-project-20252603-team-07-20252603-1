4e4fe273a9174644df5fbc1b729cbd0e82ba4920
SpencerD1123 <155921740+SpencerD1123@users.noreply.github.com>
2026-04-26 20:33:49 -0500

    Test to add a node object to graph with an element already in it passed

--- src/test/java/domain/BoardGraphTests.java
+++ src/test/java/domain/BoardGraphTests.java
@@ -18,4 +18,21 @@ public class BoardGraphTests {
         assertNotNull(b.getGraphNodeByID(0));
         EasyMock.verify();
     }
+
+    @Test
+    void addNodeToGraph_OneElementGraph_ExpectTrue(){
+        BoardGraph b = new BoardGraph();
+
+        GraphNode nodeMock1 = EasyMock.createMock(GraphNode.class);
+        GraphNode nodeMock2 = EasyMock.createMock(GraphNode.class);
+
+        EasyMock.expect(nodeMock1.getNodeID()).andReturn(0);
+        EasyMock.expect(nodeMock2.getNodeID()).andReturn(53);
+        EasyMock.replay(nodeMock1, nodeMock2);
+
+        assertTrue(b.addGraphNodeObject(nodeMock1));
+        assertTrue(b.addGraphNodeObject(nodeMock2));
+        assertNotNull(b.getGraphNodeByID(53));
+        EasyMock.verify();
+    }
 }
