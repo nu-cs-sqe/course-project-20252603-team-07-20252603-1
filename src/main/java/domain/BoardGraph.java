c64ac9c4321b96bf85b757211f64bbcdce4a6346
SpencerD1123 <155921740+SpencerD1123@users.noreply.github.com>
2026-05-02 18:35:31 -0500

    renamed maps to more JAVA appropriate expectations

--- src/main/java/domain/BoardGraph.java
+++ src/main/java/domain/BoardGraph.java
@@ -9,20 +9,20 @@ public class BoardGraph {
     // map that connects Nodes to the edges connected
 
     // map that connects nodes edges; edges themselves contain info on
-    private Map<Integer, Set<GraphEdge>> NodeID_to_Connecting_Edges = new HashMap<>();
+    private Map<Integer, Set<GraphEdge>> nodeIDToConectingEdges = new HashMap<>();
 
     // map that connects NodeID to the actual node object
-    private Map<Integer, GraphNode> NodeID_to_NodeObject = new HashMap<>();
+    private Map<Integer, GraphNode> nodeIDToNodeObject = new HashMap<>();
 
     // Add a new GraphNode to the Map
     boolean addGraphNodeObject(GraphNode graphNode) {
         int nodeID = graphNode.getNodeID();
-        if (NodeID_to_NodeObject.containsKey(nodeID)) {
+        if (nodeIDToNodeObject.containsKey(nodeID)) {
             throw new IllegalArgumentException("Node already exists");
         }
         else {
-            this.NodeID_to_NodeObject.put(nodeID, graphNode);
-            this.NodeID_to_Connecting_Edges.put(nodeID, new HashSet<>());
+            this.nodeIDToNodeObject.put(nodeID, graphNode);
+            this.nodeIDToConectingEdges.put(nodeID, new HashSet<>());
             return true;
         }
     }
@@ -30,32 +30,32 @@ public class BoardGraph {
     // Add a connecting edge to the set of edges within the map <GraphID, set of Edges>
     boolean addGraphNodeConnection(int nodeID, GraphEdge connectingEdge){
         getGraphNodeByID(nodeID); // This call works as a check that the node Exists, throwing the proper error if it does not
-        if (this.NodeID_to_Connecting_Edges.get(nodeID).contains(connectingEdge)) {
+        if (this.nodeIDToConectingEdges.get(nodeID).contains(connectingEdge)) {
             throw new IllegalArgumentException("Node already has specified edge");
         }
         else {
-            this.NodeID_to_Connecting_Edges.get(nodeID).add(connectingEdge);
+            this.nodeIDToConectingEdges.get(nodeID).add(connectingEdge);
             return true;
         }
     }
 
     // Getter function to get the graphNode Object from the map
     GraphNode getGraphNodeByID(int nodeID) {
-        if (!NodeID_to_NodeObject.containsKey(nodeID)) {
+        if (!nodeIDToNodeObject.containsKey(nodeID)) {
             throw new IllegalArgumentException("Node does not exist");
         }
         else {
-            return this.NodeID_to_NodeObject.get(nodeID);
+            return this.nodeIDToNodeObject.get(nodeID);
         }
     }
 
     // Getter function to get the set of edges from the map
     Set<GraphEdge> getConnectingEdgesByID(int nodeID) {
-        if (!NodeID_to_Connecting_Edges.containsKey(nodeID)) {
+        if (!nodeIDToConectingEdges.containsKey(nodeID)) {
             throw new IllegalArgumentException("Node does not exist");
         }
         else {
-            return this.NodeID_to_Connecting_Edges.get(nodeID);
+            return this.nodeIDToConectingEdges.get(nodeID);
         }
     }
 
