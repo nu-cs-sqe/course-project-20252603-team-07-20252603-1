4e9ee8bc1f879358c5f19a2d32368b2c6861033f
SpencerD1123 <155921740+SpencerD1123@users.noreply.github.com>
2026-05-02 14:20:50 -0500

    Test case on adding node to graph with multiple node passes

--- src/test/java/domain/BoardGraphTests.java
+++ src/test/java/domain/BoardGraphTests.java
@@ -36,4 +36,28 @@ public class BoardGraphTests {
         assertNotNull(b.getGraphNodeByID(53));
         EasyMock.verify(nodeMock1, nodeMock2);
     }
+
+    @Test
+    void addNodeToGraph_MultipleElementGraph_ExpectTrue(){
+        BoardGraph b = new BoardGraph();
+
+        GraphNode nodeMock1 = EasyMock.createMock(GraphNode.class);
+        GraphNode nodeMock2 = EasyMock.createMock(GraphNode.class);
+        GraphNode nodeMock3 = EasyMock.createMock(GraphNode.class);
+
+        EasyMock.expect(nodeMock1.getNodeID()).andReturn(0);
+        EasyMock.expect(nodeMock2.getNodeID()).andReturn(1);
+        EasyMock.expect(nodeMock3.getNodeID()).andReturn(53);
+        EasyMock.replay(nodeMock1, nodeMock2, nodeMock3);
+
+        b.addGraphNodeObject(nodeMock1);
+        b.addGraphNodeObject(nodeMock2);
+
+        assertTrue(b.addGraphNodeObject(nodeMock3));
+
+        assertNotNull(b.getGraphNodeByID(53));
+
+        EasyMock.verify(nodeMock1, nodeMock2, nodeMock3);
+
+    }
 }
