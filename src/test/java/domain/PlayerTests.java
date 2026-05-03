1691172a8b5d151bc72b7b988657769b538a9d5c
Connor Lai <connorlai@hotmail.com>
2026-05-03 18:19:09 -0500

    PlaceSettlement_OnUnoccupiedVertexNoAdjacentSettlementsZeroSettlements_ExpectLenOne passes

--- src/test/java/domain/PlayerTests.java
+++ src/test/java/domain/PlayerTests.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,5 +16,27 @@ public class PlayerTests {
             player.placeSettlement(vertex);
         });
         assertEquals("Vertex cannot be null", exception.getMessage());
+    }
+    @Test
+    public void PlaceSettlement_OnUnoccupiedVertexNoAdjacentSettlementsZeroSettlements_ExpectLenOne() {
+        final int expectedNumSettlementsAfterPlace = 1;
+
+        // mock vertex
+        Vertex vertex = EasyMock.createMock(Vertex.class);
+        EasyMock.expect(vertex.isOccupied()).andReturn(false);
+        EasyMock.expect(vertex.hasAdjacentSettlementViolatingDistanceRule()).andReturn(false);
+        EasyMock.replay(vertex);
+
+        // create player
+        Player player = new Player();
+        player.placeSettlement(vertex);
+
+        assertEquals(
+            expectedNumSettlementsAfterPlace,
+            player.getSettlements().size(),
+            "expected: settlement appended to player's settlements list"
+        );
+
+        EasyMock.verify(vertex);
     }
 }
