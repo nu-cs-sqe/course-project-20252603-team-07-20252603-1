faeae23f52e83ace03cc9fce6ee9758d39c78515
kdawg <kenwang3333@gmail.com>
2026-05-05 21:09:48 -0500

    testIsNameAvailableForUnusedNameReturnsTrue passes

---
+++ src/test/java/domain/model/GameSetupModelTest.java
@@ -0,0 +1,21 @@
+package domain.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameSetupModelTest {
+
+    private GameSetupModel model;
+
+    @BeforeEach
+    void setUp() {
+        model = new GameSetupModel();
+    }
+
+    @Test
+    void testIsNameAvailableForUnusedNameReturnsTrue() {
+        assertTrue(model.isNameAvailable("Alice"));
+    }
+}
