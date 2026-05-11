82d3aa7dadf5811c35848d0f8e1d0f8216cf0802
theo <thjmaurino@gmail.com>
2026-05-10 19:30:16 -0500

    feat: implement GameModel with turn order tracking

--- src/main/java/domain/model/GameModel.java
+++ src/main/java/domain/model/GameModel.java
@@ -1,5 +1,16 @@
 package domain.model;
 
+import java.util.List;
+
 public class GameModel {
-    
+
+    private final List<Player> turnOrder;
+
+    public GameModel(List<Player> turnOrder) {
+        this.turnOrder = turnOrder;
+    }
+
+    public List<Player> getTurnOrder() {
+        return turnOrder;
+    }
 }
