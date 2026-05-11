89a78c5f13a240118ea3242c60f7a6d453cff451
SpencerD1123 <155921740+SpencerD1123@users.noreply.github.com>
2026-05-10 22:14:49 -0500

    checkPlayerOwnsNeighboringEdges_test02_WhiteOwnsEdgeConnectingToEndingNode_ExpectTrue() passes, no change to production code needed

--- src/test/java/domain/BoardGraphTests.java
+++ src/test/java/domain/BoardGraphTests.java
@@ -679,7 +679,7 @@ public class BoardGraphTests {
 
     // checkPlayerOwnsNeighboringEdges() tests
     @Test
-    void checkPlayerOwnsNeighboringEdges_RedOwnsEdgeConnectingToStartingNode_ExpectTrue(){
+    void checkPlayerOwnsNeighboringEdges_test01_RedOwnsEdgeConnectingToStartingNode_ExpectTrue(){
         BoardGraph b = new BoardGraph();
 
         GraphNode nodeStub0 = EasyMock.createNiceMock(GraphNode.class);
@@ -703,6 +703,37 @@ public class BoardGraphTests {
         b.addGraphNodeObject(nodeStub1);
         b.addGraphNodeConnection(0, edge0to1);
         b.addGraphNodeConnection(0, edge0to2);
+        b.addGraphNodeConnection(1, edge0to1);
+
+        assertTrue(b.checkPlayerOwnsNeighboringEdge(PlayerColor.RED, 0, 1));
+
+    }
+
+    @Test
+    void checkPlayerOwnsNeighboringEdges_test02_WhiteOwnsEdgeConnectingToEndingNode_ExpectTrue(){
+        BoardGraph b = new BoardGraph();
+
+        GraphNode nodeStub0 = EasyMock.createNiceMock(GraphNode.class);
+        GraphNode nodeStub1 = EasyMock.createNiceMock(GraphNode.class);
+        GraphEdge edge0to1 = EasyMock.createNiceMock(GraphEdge.class);
+        // edge which red owns
+        GraphEdge edge1to2 = EasyMock.createNiceMock(GraphEdge.class);
+
+        EasyMock.expect(nodeStub0.getNodeID()).andStubReturn(0);
+        EasyMock.expect(nodeStub1.getNodeID()).andStubReturn(1);
+
+        EasyMock.expect(edge0to1.getStartingNodeID()).andStubReturn(0);
+        EasyMock.expect(edge0to1.getEndingNodeID()).andStubReturn(1);
+
+        EasyMock.expect(edge1to2.getStartingNodeID()).andStubReturn(1);
+        EasyMock.expect(edge1to2.getEndingNodeID()).andStubReturn(2);
+        EasyMock.expect(edge1to2.checkOwningColor()).andStubReturn(PlayerColor.WHITE);
+        EasyMock.replay(nodeStub0, nodeStub1, edge0to1, edge1to2);
+
+        b.addGraphNodeObject(nodeStub0);
+        b.addGraphNodeObject(nodeStub1);
+        b.addGraphNodeConnection(0, edge0to1);
+        b.addGraphNodeConnection(0, edge1to2);
         b.addGraphNodeConnection(1, edge0to1);
 
         assertTrue(b.checkPlayerOwnsNeighboringEdge(PlayerColor.RED, 0, 1));
