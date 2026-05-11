07251d1c2edfe42a712a89f6dd0e5df154dd5002
theo <thjmaurino@gmail.com>
2026-05-10 19:35:20 -0500

    feat: add current player index tracking to GameModel

--- src/main/java/domain/model/GameModel.java
+++ src/main/java/domain/model/GameModel.java
@@ -5,12 +5,18 @@ import java.util.List;
 public class GameModel {
 
     private final List<Player> turnOrder;
+    private int currentPlayerIndex;
 
     public GameModel(List<Player> turnOrder) {
         this.turnOrder = turnOrder;
+        this.currentPlayerIndex = 0;
     }
 
     public List<Player> getTurnOrder() {
         return turnOrder;
+    }
+
+    public int getCurrentPlayerIndex() {
+        return currentPlayerIndex;
     }
 }
