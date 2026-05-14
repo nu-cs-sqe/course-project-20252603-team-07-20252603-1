2f438d8d7cc6930fbb8b687de18e5591e0ee8a76
benauby11 <benjaminauby2026@u.northwestern.edu>
2026-05-14 17:49:42 -0500

    Removed magic number

--- src/main/java/domain/Die.java
+++ src/main/java/domain/Die.java
@@ -3,13 +3,16 @@ package domain;
 import java.util.Random;
 
 class Die {
+    private static final int DIE_SIDES_NUMBER = 6;
+
     private final Random randomizer;
 
     Die(Random randomizer) {
         this.randomizer = randomizer;
     }
 
+    // Need to add 1 to result of nextInt, returns in range of [0, bound)
     int rollOneDie(){
-        return randomizer.nextInt(6) + 1;
+        return randomizer.nextInt(DIE_SIDES_NUMBER) + 1;
     }
 }
