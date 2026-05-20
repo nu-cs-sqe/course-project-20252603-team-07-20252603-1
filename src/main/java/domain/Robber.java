3d7cbb7eec4a01b6a0050f6c8048f6557b290582
benauby11 <benjaminauby2026@u.northwestern.edu>
2026-05-19 23:31:32 -0500

    removed unused variable

--- src/main/java/domain/Robber.java
+++ src/main/java/domain/Robber.java
@@ -1,10 +1,9 @@
 package domain;
 
 public class Robber {
-    private int robberCurrentHexId;
 
     public Robber(){
-        robberCurrentHexId = 9; // initialize robber to be on desert tile
+
     }
 
     void getRobberLocation(){
