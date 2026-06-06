495266c972838473053afb308cc31aec9a516403
SpencerD1123 <155921740+SpencerD1123@users.noreply.github.com>
2026-06-06 16:55:03 -0500

    changed resource tracking from list to Map to correspond with original player implentation which used a map

--- src/main/java/domain/model/player/PlayerState.java
+++ src/main/java/domain/model/player/PlayerState.java
@@ -2,8 +2,7 @@ package domain.model.player;
 
 import domain.model.resources.Resource;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Represents mutable game state for a player.
@@ -13,15 +12,26 @@ public class PlayerState {
 
     private final Player player;
     private final PlayerColor color;
-    private final List<Resource> resources;
+    //private final List<Resource> resources;
+    private final Map<Resource, Integer> resourceCount = new HashMap<>();
     private int numSettlement;
 
     public PlayerState(Player player) {
         this.player = player;
         this.color = player.getColor();
-        this.resources = new ArrayList<>();
+        //this.resources = new ArrayList<>();
+        // Connor and Ben have been defining methods for interacting
+        // With the player class using the Player (not PlayerState) class
+        // As such, the way that keeps track of resources is by using a Map
+        // From resource Type to int (i.e. wood -> 3)
+        // I've edited the methods below to use this map
+        for (Resource r : Resource.values()){
+            resourceCount.put(r, 0);
+        }
         numSettlement = 0;
     }
+
+    //
 
     public Player getPlayer() {
         return player;
@@ -30,14 +40,27 @@ public class PlayerState {
     public PlayerColor getColor(){
         return this.color;
     }
-    public void addResource(Resource card) {
-        resources.add(card);
-    }
 
+    // Resources with List
+//    public void addResource(Resource card) {
+//        resources.add(card);
+//    }
+
+//    public int getResourceCount(Resource type) {
+//        return (int) resources.stream()
+//                .filter(card -> card == type)
+//                .count();
+//    }
+
+    // Resources with Map
+
+    public void addResource(Resource type) {
+        int oldCount = getResourceCount(type);
+        int newCount = oldCount + 1;
+        resourceCount.put(type, newCount);
+    }
     public int getResourceCount(Resource type) {
-        return (int) resources.stream()
-                .filter(card -> card == type)
-                .count();
+        return resourceCount.get(type);
     }
 
     // Defined while working on GameModel
