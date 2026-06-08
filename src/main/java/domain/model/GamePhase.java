c8358f1ac17f327f836d2e8f72b43571fb334e81
SpencerD1123 <155921740+SpencerD1123@users.noreply.github.com>
2026-06-07 20:49:24 -0500

    Added more game phases

--- src/main/java/domain/model/GamePhase.java
+++ src/main/java/domain/model/GamePhase.java
@@ -4,4 +4,8 @@ public enum GamePhase {
     RESOURCE_PRODUCTION, //  Beginning of turn
     MOVE_ROBBER, // Player rolls a 7, or plays a dev card, must immediately move robber
     GENERAL_PLAY,
+    MONOPOLY_DEV_CARD,
+    BUILDING_ROAD,
+    BUILDING_SETTLEMENT,
+    OFFERING_TRADE
 }
