9680893d31e39076848897643eb76c29ee658396
Connor Lai <connorlai@hotmail.com>
2026-06-09 15:22:39 -0500

    buyDevelopmentCard_BuyerHasSurplusResources_ExpectCardDrawnAndResourcesDecremented passes

--- src/test/java/domain/model/DevelopmentCardHandlerTest.java
+++ src/test/java/domain/model/DevelopmentCardHandlerTest.java
@@ -20,7 +20,6 @@ class DevelopmentCardHandlerTest {
     final int currentRound = 1;
 
     Player mockBuyer = EasyMock.createMock(Player.class);
-
     DevelopmentCardDeck mockDeck = EasyMock.createMock(DevelopmentCardDeck.class);
     DevelopmentCard mockCard = EasyMock.createMock(DevelopmentCard.class);
 
@@ -28,7 +27,40 @@ class DevelopmentCardHandlerTest {
     EasyMock.expect(mockBuyer.getResourceCount(Resource.WOOL)).andReturn(1);
     EasyMock.expect(mockBuyer.getResourceCount(Resource.GRAIN)).andReturn(1);
     EasyMock.expect(mockDeck.drawCard(currentRound)).andReturn(mockCard);
+    
+    mockBuyer.updateResources(Resource.ORE, -1);
+    EasyMock.expectLastCall();
+    mockBuyer.updateResources(Resource.WOOL, -1);
+    EasyMock.expectLastCall();
+    mockBuyer.updateResources(Resource.GRAIN, -1);
+    EasyMock.expectLastCall();
+    mockBuyer.addDevelopmentCard(mockCard);
+    EasyMock.expectLastCall();
 
+    EasyMock.replay(mockBuyer, mockDeck, mockCard);
+
+    DevelopmentCardHandler handler = new DevelopmentCardHandler();
+    DevelopmentCard result = handler.buyDevelopmentCard(mockBuyer, mockDeck, currentRound);
+
+    EasyMock.verify(mockBuyer, mockDeck, mockCard);
+    assertEquals(mockCard, result);
+  }
+
+  // TC2: buyer has 3 ORE, 2 WOOL, 4 GRAIN (surplus); deck full (25)
+  //      -> card returned; buyer's resources each decremented by 1; card added to hand
+  @Test
+  void buyDevelopmentCard_BuyerHasSurplusResources_ExpectCardDrawnAndResourcesDecremented() throws EmptyDeckException {
+    final int currentRound = 1;
+
+    Player mockBuyer = EasyMock.createMock(Player.class);
+    DevelopmentCardDeck mockDeck = EasyMock.createMock(DevelopmentCardDeck.class);
+    DevelopmentCard mockCard = EasyMock.createMock(DevelopmentCard.class);
+
+    EasyMock.expect(mockBuyer.getResourceCount(Resource.ORE)).andReturn(3);
+    EasyMock.expect(mockBuyer.getResourceCount(Resource.WOOL)).andReturn(2);
+    EasyMock.expect(mockBuyer.getResourceCount(Resource.GRAIN)).andReturn(4);
+    EasyMock.expect(mockDeck.drawCard(currentRound)).andReturn(mockCard);
+    
     mockBuyer.updateResources(Resource.ORE, -1);
     EasyMock.expectLastCall();
     mockBuyer.updateResources(Resource.WOOL, -1);
