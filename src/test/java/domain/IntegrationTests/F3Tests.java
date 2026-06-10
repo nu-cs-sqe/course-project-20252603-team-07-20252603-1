b3c8a1b977ad29accd320b82c9a3ccfc3f6ce9f3
benauby11 <benjaminauby2026@u.northwestern.edu>
2026-06-10 00:41:46 -0500

    BVA complete for gam Feature 3

---
+++ src/test/java/domain/IntegrationTests/F3Tests.java
@@ -0,0 +1,7 @@
+package domain.IntegrationTests;
+
+// Tests for Feature 3:
+// Ability to place initial settlements and roads during the setup phase according to setup rules
+
+public class F3Tests {
+}
