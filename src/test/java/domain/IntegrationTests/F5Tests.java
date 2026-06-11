b15fc872d82843c14cb1087a3d2c2d6b57e53413
benauby11 <benjaminauby2026@u.northwestern.edu>
2026-06-10 23:50:43 -0500

    TwoRolled_RobberNotOnHex_NoSettlementsOrCities_NoResourcesAwarded passes

--- src/test/java/domain/IntegrationTests/F5Tests.java
+++ src/test/java/domain/IntegrationTests/F5Tests.java
@@ -3,9 +3,14 @@ package domain.IntegrationTests;
 // Tests for Feature 5:
 // Ability to roll dice and distribute resources to players based on settlements and cities adjacent to matching number tokens, excluding robber-blocked hexes
 
+import domain.model.board.BoardHandler;
 import domain.model.game_pieces.DiceHandler;
+import domain.model.player.Player;
+import domain.model.player.PlayerColor;
+import domain.model.resources.Resource;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class F5Tests {
@@ -15,6 +20,22 @@ public class F5Tests {
     DiceHandler diceHandler = new DiceHandler();
     int result = diceHandler.rollTwoDice();
     assertTrue(result >= 2 && result <= 12);
+  }
+
+  // Test Case 2
+  @Test
+  void TwoRolled_RobberNotOnHex_NoSettlementsOrCities_NoResourcesAwarded() {
+    BoardHandler b = new BoardHandler();
+
+    Player orangePlayer = new Player("Dummy", PlayerColor.ORANGE);
+
+    b.awardResources(2);
+
+    assertEquals(0, orangePlayer.getResourceCount(Resource.WOOL));
+    assertEquals(0, orangePlayer.getResourceCount(Resource.LUMBER));
+    assertEquals(0, orangePlayer.getResourceCount(Resource.ORE));
+    assertEquals(0, orangePlayer.getResourceCount(Resource.GRAIN));
+    assertEquals(0, orangePlayer.getResourceCount(Resource.BRICK));
   }
 
 
