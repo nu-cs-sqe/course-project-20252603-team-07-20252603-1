106a89d46398b29529df334a79aae90809332d84
benauby11 <benjaminauby2026@u.northwestern.edu>
2026-06-10 23:48:50 -0500

    DiceHandlerRolled_InRangeTwoToTwelve passes

--- src/main/java/domain/model/game_pieces/DiceHandler.java
+++ src/main/java/domain/model/game_pieces/DiceHandler.java
@@ -1,12 +1,21 @@
 package domain.model.game_pieces;
 
+import java.util.Random;
+
 public class DiceHandler {
     private final Die dieOne;
     private final Die dieTwo;
 
-    public DiceHandler(Die firstDie, Die secondDie) {
+    // private constructor for tests, with injection
+    DiceHandler(Die firstDie, Die secondDie) {
         this.dieOne = firstDie;
         this.dieTwo = secondDie;
+    }
+
+    // public constructor for usage
+    public DiceHandler(){
+        this.dieOne = new Die(new Random());
+        this.dieTwo = new Die(new Random());
     }
 
     // Add two separate Die rolls together
--- src/test/java/domain/IntegrationTests/F5Tests.java
+++ src/test/java/domain/IntegrationTests/F5Tests.java
@@ -3,7 +3,20 @@ package domain.IntegrationTests;
 // Tests for Feature 5:
 // Ability to roll dice and distribute resources to players based on settlements and cities adjacent to matching number tokens, excluding robber-blocked hexes
 
+import domain.model.game_pieces.DiceHandler;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class F5Tests {
+  // Test Case 1 - rollTwoDice
+  @Test
+  void DiceHandlerRolled_InRangeTwoToTwelve() {
+    DiceHandler diceHandler = new DiceHandler();
+    int result = diceHandler.rollTwoDice();
+    assertTrue(result >= 2 && result <= 12);
+  }
+
 
 }
 
