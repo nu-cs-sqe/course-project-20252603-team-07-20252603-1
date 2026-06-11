c56051cb1dfd019f744abee3b14a9c862b904da7
benauby11 <benjaminauby2026@u.northwestern.edu>
2026-06-10 23:27:13 -0500

    BVA complete for Feature 5 - Awarding Resources

---
+++ src/test/java/domain/IntegrationTests/F5Tests.java
@@ -0,0 +1,10 @@
+package domain.IntegrationTests;
+
+// Tests for Feature 5:
+// Ability to roll dice and distribute resources to players based on settlements and cities adjacent to matching number tokens, excluding robber-blocked hexes
+
+public class F5Tests {
+
+}
+
+
