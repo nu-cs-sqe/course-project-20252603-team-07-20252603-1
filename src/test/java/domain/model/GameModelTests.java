6f0b1aa558c28f308ae0db7b43be2b5080ff2aa8
Connor Lai <connorlai@hotmail.com>
2026-06-12 13:40:49 -0500

    buyDevCard_ExactResources_LastCardInDeck_ExpectCardReturnedAndResourcesDeducted passes

--- src/test/java/domain/model/GameModelTests.java
+++ src/test/java/domain/model/GameModelTests.java
@@ -1621,4 +1621,35 @@ public class GameModelTests {
 
         EasyMock.verify(redStateMock, deckMock, cardMock, oreDeckMock, woolDeckMock, grainDeckMock);
     }
+
+    // TC3: GENERAL_PLAY, ORE=1, WOOL=1, GRAIN=1, deck=1 (last card)
+    //      -> card returned; deck countRemaining = 0
+    @Test
+    void buyDevCard_ExactResources_LastCardInDeck_ExpectCardReturnedAndResourcesDeducted() throws EmptyDeckException {
+        DevelopmentCardDeck deckMock = EasyMock.createMock(DevelopmentCardDeck.class);
+        DevelopmentCard cardMock = EasyMock.createMock(DevelopmentCard.class);
+        Player redStateMock = EasyMock.createMock(Player.class);
+        ColorToPlayerObjMock = Map.of(PlayerColor.RED, redStateMock);
+
+        EasyMock.expect(deckMock.drawCard(0)).andReturn(cardMock);
+        redStateMock.updateResources(Resource.ORE, -1);
+        redStateMock.updateResources(Resource.WOOL, -1);
+        redStateMock.updateResources(Resource.GRAIN, -1);
+        oreDeckMock.replenish();
+        woolDeckMock.replenish();
+        grainDeckMock.replenish();
+        redStateMock.addDevelopmentCard(cardMock);
+
+        EasyMock.replay(redStateMock, deckMock, cardMock, oreDeckMock, woolDeckMock, grainDeckMock);
+
+        GameModel model = new GameModel(lumberDeckMock, brickDeckMock, grainDeckMock,
+                oreDeckMock, woolDeckMock, ColorToPlayerObjMock, boardMock);
+        model.setCurrentPlayerColor(PlayerColor.RED);
+        model.setCurrentGamePhase(GamePhase.GENERAL_PLAY);
+
+        DevelopmentCard result = model.buyDevCard(deckMock);
+        assertEquals(cardMock, result);
+
+        EasyMock.verify(redStateMock, deckMock, cardMock, oreDeckMock, woolDeckMock, grainDeckMock);
+    }
 }
